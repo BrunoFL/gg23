1c7f3c73f130e8156e97e7d1f0e4460c2a821f86
BrunoFL <follet-bruno@hotmail.fr>
2023-01-31 18:06:35 +0100

    datasource

--- src/main/java/com/fapguys/studio/ggj23/Ggj23Application.java
+++ src/main/java/com/fapguys/studio/ggj23/Ggj23Application.java
@@ -4,6 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+import java.util.Map;
 
 @SpringBootApplication
 public class Ggj23Application {
@@ -13,5 +18,14 @@ public class Ggj23Application {
 		SpringApplication.run(Ggj23Application.class, args);
 	}
 
-
+	@Bean
+	public DataSource dataSource() {
+		Map<String, String> env = System.getenv();
+		return DataSourceBuilder.create()
+				.driverClassName("org.postgresql.Driver")
+				.url(env.getOrDefault("POSTGRESQL_ADDON_URI", "jdbc:postgresql://localhost:5433/ggj23"))
+				.username(env.getOrDefault("POSTGRESQL_ADDON_USER", "postgres"))
+				.password(env.getOrDefault("POSTGRESQL_ADDON_PASSWORD", "pass"))
+				.build();
+	}
 }
