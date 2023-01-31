1d42c5127afe1d7ecabb37aed2f6857f803f3394
BrunoFL <follet-bruno@hotmail.fr>
2023-01-31 15:17:43 +0100

    base

--- src/main/java/com/fapguys/studio/ggj23/model/UserRepository.java
+++ src/main/java/com/fapguys/studio/ggj23/model/UserRepository.java
@@ -1,13 +1,15 @@
 package com.fapguys.studio.ggj23.model;
 
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.ListCrudRepository;
 
 import java.util.List;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends ListCrudRepository<User, Long> {
 
     List<User> findByLastName(String lastName);
 
     User findById(long id);
+
+    List<User> findAll();
 }
