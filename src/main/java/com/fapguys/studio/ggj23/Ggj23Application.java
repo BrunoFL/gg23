9252876da6628096fe76421b8b2a24d9e241c615
BrunoFL <follet-bruno@hotmail.fr>
2023-01-31 15:08:37 +0100

    je suis un con

--- src/main/java/com/fapguys/studio/ggj23/Ggj23Application.java
+++ src/main/java/com/fapguys/studio/ggj23/Ggj23Application.java
@@ -1,13 +1,22 @@
 package com.fapguys.studio.ggj23;
 
+import com.fapguys.studio.ggj23.model.User;
+import com.fapguys.studio.ggj23.model.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Ggj23Application {
+	private static final Logger log = LoggerFactory.getLogger(Ggj23Application.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ggj23Application.class, args);
 	}
+
 
 }
---
+++ src/main/java/com/fapguys/studio/ggj23/model/UserRepository.java
@@ -0,0 +1,13 @@
+package com.fapguys.studio.ggj23.model;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    List<User> findByLastName(String lastName);
+
+    User findById(long id);
+}
