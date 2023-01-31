16604179c00790be5e4b28625729def6c5b08ba7
BrunoFL <follet-bruno@hotmail.fr>
2023-01-31 14:00:19 +0100

    base

---
+++ src/main/java/com/fapguys/studio/ggj23/Ggj23Application.java
@@ -0,0 +1,13 @@
+package com.fapguys.studio.ggj23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ggj23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ggj23Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/fapguys/studio/ggj23/Ggj23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fapguys.studio.ggj23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ggj23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
