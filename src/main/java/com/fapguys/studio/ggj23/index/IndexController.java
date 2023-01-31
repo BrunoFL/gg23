14d794fce53cdf4af59b4651cbd5d64b791b013e
BrunoFL <follet-bruno@hotmail.fr>
2023-01-31 21:19:48 +0100

    remove test

--- src/main/java/com/fapguys/studio/ggj23/index/IndexController.java
+++ src/main/java/com/fapguys/studio/ggj23/index/IndexController.java
@@ -1,12 +1,18 @@
 package com.fapguys.studio.ggj23.index;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class IndexController {
+public class IndexController implements ErrorController {
     @GetMapping("/")
     public String index() {
+        return "index";
+    }
+
+    @GetMapping("/error")
+    public String error(){
         return "index";
     }
 }
